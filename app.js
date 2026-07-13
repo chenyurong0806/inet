024dd83bde12ea2e2c2ba0ff2f84ad5a1a47ad63
chenyurong0806 <chenyurong0806@gmail.com>
2026-07-13 17:11:49 +0800

    V1.0

---
+++ app.js
@@ -0,0 +1,42 @@
+const API =
+    "https://inet.chenyurong0806.workers.dev/";
+
+
+
+async function upload() {
+
+
+    let file =
+        document
+            .getElementById("file")
+            .files[0];
+
+
+    let data =
+        new FormData();
+
+
+    data.append(
+        "file",
+        file
+    );
+
+
+
+    await fetch(
+        API + "/upload",
+        {
+
+            method: "POST",
+
+            body: data
+
+        });
+
+
+    alert(
+        "上传成功"
+    );
+
+
+}
